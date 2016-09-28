347d23570c5621ca3e23a90aaf5d4b1274635f7f
Marcelo Rodrigo <marcelo.rodrigo@fluig.com>
2016-09-28 12:33:10 -0300

    @kassner code using Angular1

---
+++ js/calculadora.js
@@ -0,0 +1,162 @@
+var RendaFixaApp = angular.module('RendaFixaApp', []);
+
+RendaFixaApp.controller('CalculatorController', function ($scope, $http) {
+
+    $http.get('taxes.json').success(function (data) {
+        $scope.selic = data.selic;
+        $scope.cdi = data.cdi;
+
+        $scope.calculate();
+    });
+
+    $scope.cdbTax = 83;
+    $scope.lcxTax = 90;
+    $scope.amount = 1000;
+    $scope.period = 181;
+
+    $scope.calculate = function () {
+        $scope.poupanca = PoupancaType.calculate($scope.amount, $scope.period);
+        $scope.cdb = CDBType.calculate($scope.cdi, $scope.amount, $scope.period, $scope.cdbTax);
+        $scope.lcx = LCxType.calculate($scope.cdi, $scope.amount, $scope.period, $scope.lcxTax);
+        $scope.tesouroselic = TesouroSelicType.calculate($scope.selic, $scope.amount, $scope.period);
+        $scope.percent = AbstractType.calculatePercent($scope);
+    };
+});
+
+/**
+ * @TODO do not allow negative amount on any type
+ * @TODO do not allow negative period on any type
+ */
+
+var PoupancaType = {
+    calculate: function (amount, period) {
+        var months = AbstractType.getPeriodInMonths(period);
+        var net = math.round(math.pow(1.005, months) * amount, 2);
+
+        return {
+            'net': net,
+            'interest': math.round(net - amount, 2)
+        };
+    }
+};
+
+var CDBType = {
+    calculate: function (cdi, amount, period, percent) {
+        var months = AbstractType.getPeriodInMonths(period);
+        var tax = math.pow((((cdi * (percent / 100)) / 100) + 1), (1 / 12));
+
+        var gross = math.round(math.pow(tax, months) * amount, 2);
+        var iof = AbstractType.calculateIof(gross - amount, period);
+        var irpf = AbstractType.calculateIrpf(gross - amount, period);
+        var net = math.round(gross - iof - irpf.amount, 2);
+
+        return {
+            'gross': gross,
+            'irpf': irpf,
+            'iof': iof,
+            'net': net,
+            'interest': math.round(net - amount, 2)
+        };
+    }
+};
+
+var LCxType = {
+    calculate: function (cdi, amount, period, percent) {
+        var months = AbstractType.getPeriodInMonths(period);
+        var rate = math.pow((((cdi * (percent / 100)) / 100) + 1), (1 / 12));
+        var net = math.round(math.pow(rate, months) * amount, 2);
+
+        return {
+            'net': net,
+            'interest': math.round(net - amount, 2)
+        };
+    }
+};
+
+var TesouroSelicType = {
+    cblc: 0.3,
+
+    calculate: function (selic, amount, period) {
+        var months = AbstractType.getPeriodInMonths(period);
+        var tax = math.pow(((selic / 100) + 1), (1 / 12));
+
+        var gross = math.round(math.pow(tax, months) * amount, 2);
+        var iof = AbstractType.calculateIof(gross - amount, period);
+        var irpf = AbstractType.calculateIrpf(gross - amount, period);
+        var cblc = this.calculateCblc(amount, period);
+        var net = math.round(gross - iof - irpf.amount - cblc, 2);
+
+        return {
+            'gross': gross,
+            'irpf': irpf,
+            'iof': iof,
+            'cblc': cblc,
+            'net': net,
+            'interest': math.round(net - amount, 2)
+        };
+    },
+
+    calculateCblc: function (amount, period) {
+        var semesters = math.ceil(period / 180);
+        return amount * ((this.cblc / 100) / 2) * semesters;
+    }
+};
+
+var AbstractType = {
+    getPeriodInMonths: function (period) {
+        if (period < 0) {
+            return 0;
+        }
+
+        return math.floor(period / 30);
+    },
+
+    calculateIof: function (amount, period) {
+        if (period <= 0 || period >= 30) {
+            return 0;
+        }
+
+        if (amount == 0) {
+            return 0;
+        }
+
+        var percent = (100 - math.ceil(period * 10 / 3)) / 100;
+        return math.round(percent * amount, 2);
+    },
+
+    calculateIrpf: function (amount, period) {
+        var branch = 22.5;
+
+        if (period > 720) {
+            branch = 15;
+        } else if (period > 360) {
+            branch = 17.5;
+        } else if (period > 180) {
+            branch = 20;
+        }
+
+        var taxAmount = math.round(amount * (branch / 100), 2);
+
+        return {
+            'branch': branch,
+            'amount': taxAmount
+        }
+    },
+
+    calculatePercent: function (scope) {
+        var netValues = [scope.lcx.interest, scope.cdb.interest, scope.tesouroselic.interest, scope.poupanca.interest];
+        var max = math.max(netValues) / 100;
+
+        if (max === 0) {
+            max = 1;
+        }
+
+        return {
+            'lcx': math.round(scope.lcx.interest / max, 2),
+            'cdb': math.round(scope.cdb.interest / max, 2),
+            'poupanca': math.round(scope.poupanca.interest / max, 2),
+            'tesouroselic': math.round(scope.tesouroselic.interest / max, 2)
+        };
+    }
+
+}
