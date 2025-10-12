723aa80a2be8a06dcc944bb5c0fd3cd5931f218e
Levi Lucena <sr_lucen@yahoo.com.br>
2025-10-12 14:34:29 -0300

    fix: correções finais para resolver problemas de hidratação e renderização

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,19 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <InvestmentInfo />
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col cols="12" sm="12" md="5" lg="4">
-      <InvestmentInput />
-    </v-col>
-    <v-col cols="12" sm="12" md="7" lg="8">
-      <InvestmentSimulation />
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <InvestmentInfo />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="12" md="5" lg="4">
+        <InvestmentInput />
+      </v-col>
+      <v-col cols="12" sm="12" md="7" lg="8">
+        <InvestmentSimulation />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script setup lang='ts'>
 useSeoMeta({
