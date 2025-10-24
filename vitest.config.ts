5eb1551b1c8f57b106b324a50e5e5a2fa3ca8066
Marcelo Wiebbelling <mrodrigow@gmail.com>
2025-10-24 13:07:13 +0200

    The async arrow function should be named

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config'
 import { defineVitestProject } from '@nuxt/test-utils/config'
 
-export default async () => {
+export default async function defineVitestConfig() {
   return defineConfig({
     test: {
       projects: [
